ea9b2954f22326a1dc688995c307417eef6cf930
Shannon Beach <shannonbeach@Shannons-MBP.attlocal.net>
2017-01-07 13:05:40 -0500

    refactored code. Added functions file. Moved code to echo list to display function

--- catalog.php
+++ catalog.php
@@ -1,5 +1,7 @@
 <?php
 include ("includes/data.php");
+include ("includes/functions.php");
+
 $pageTitle = "Full Catalog";
 $section = null;
 
@@ -27,11 +29,8 @@ include("includes/header.php");
         <h1><?php echo $pageTitle; ?></h1>
         <ul class="items">
             <?php
-            foreach ($catalog as $item) {
-                echo "<li><a href='#'><img src='" . $item["img"] ."'
-                                            alt='". $item["title"] . "' />"
-                                                  ."<p> View Details</p> "
-                                                  ."</a></li>";
+            foreach ($catalog as $id => $item) {
+                echo getItemHtml($id, $item);
             } ?>
         </ul>
     </div>
--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
+include("includes/data.php");
+include("includes/functions.php");
+
 $pageTitle = "Home";
 $section = null;
+
 include ("includes/header.php"); ?>
 
 		<div class="section catalog random">
@@ -10,7 +14,10 @@ include ("includes/header.php"); ?>
 				<h2>May we suggest something?</h2>
 
 				<ul class="items">
-					<li><a href="details.php?id=201"><img src="img/media/forest_gump.jpg" alt="Forrest Gump"><p>View Details</p></a></li><li><a href="details.php?id=204"><img src="img/media/princess_bride.jpg" alt="The Princess Bride"><p>View Details</p></a></li><li><a href="details.php?id=302"><img src="img/media/elvis_presley.jpg" alt="Elvis Forever"><p>View Details</p></a></li><li><a href="details.php?id=303"><img src="img/media/garth_brooks.jpg" alt="No Fences"><p>View Details</p></a></li>
+					<?php
+					foreach ($catalog as $id => $item) {
+						echo getItemHtml($id, $item);
+					} ?>
 				</ul>
 
 			</div>
